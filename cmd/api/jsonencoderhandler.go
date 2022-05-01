56debe7742999b67bb4a97058fe5c398bfa02d36
eRaBxEs <ehiojee@yahoo.com>
2022-05-01 18:29:36 +0100

    add comment for json encoder example

--- cmd/api/jsonencoderhandler.go
+++ cmd/api/jsonencoderhandler.go
@@ -7,15 +7,16 @@ import (
 
 func (app *application) exampleJsonEncoderHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]string{
-		"hello": "world",
-	}
-
+		"hello": "world"}
+	// Set the "Content-Type: application/json" header on the response.
 	w.Header().Set("Content-Type", "application/json")
-
+	// Use the json.NewEncoder() function to initialize a json.Encoder instance that
+	// writes to the http.ResponseWriter. Then we call its Encode() method, passing in // the data that we want to encode to JSON (which in this case is the map above). If // the data can be successfully encoded to JSON, it will then be written to our
+	// http.ResponseWriter.
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and so could not process your request", http.StatusInternalServerError)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
 
 }
