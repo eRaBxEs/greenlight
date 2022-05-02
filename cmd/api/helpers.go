38e9b25e646fa06003649645cd113213fc862f7b
eRaBxEs <ehiojee@yahoo.com>
2022-05-02 14:02:41 +0100

    envelope json responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -20,10 +20,11 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-// Define a writeJSON() helper for sending responses. This takes the destination
-// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
-// header map containing any additional HTTP headers we want to include in the response.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+// Define an envelope type.
+type envelope map[string]interface{}
+
+// Change the data parameter to have the type envelope instead of interface{}.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Use the json.MarshalIndent() function so that whitespace is added to the encoded
 	// JSON. Here we use no line prefix ("") and tab indents ("\t") for each element.
 	js, err := json.MarshalIndent(data, "", "\t")
